7d29d81c17a25f9cbebdd368b0ce97306ad41ef1
2sagarraut2 <https://github.com/2sagarraut2/helloworld.git>
2022-02-18 23:35:50 +0530

    unwanted code removed

--- src/container/BatchControl/Batch.js
+++ src/container/BatchControl/Batch.js
@@ -1,49 +1,12 @@
-import React, { useState } from 'react';
-import { Snackbar } from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import Loader from '../../component/Loader';
+import React from 'react';
 import '../../styles/batchcontrol.scss';
 import Batches from '../../component/BatchController/Batches';
 
 const Batch = () => {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [successMsg, setSuccessMsg] = useState('');
-	const [errorMsg, setErrorMsg] = useState('');
-
 	return (
 		<div>
-			<Snackbar
-				open={Boolean(successMsg)}
-				autoHideDuration={3000}
-				onClose={() => setSuccessMsg('')}>
-				<Alert
-					elevation={6}
-					variant='filled'
-					onClose={() => {
-						setSuccessMsg('');
-					}}
-					severity='success'>
-					{successMsg}
-				</Alert>
-			</Snackbar>
-			<Snackbar
-				open={Boolean(errorMsg)}
-				autoHideDuration={3000}
-				onClose={() => setErrorMsg('')}>
-				<Alert
-					elevation={6}
-					variant='filled'
-					onClose={() => {
-						setErrorMsg('');
-					}}
-					severity='error'>
-					{errorMsg}
-				</Alert>
-			</Snackbar>
-			{loading && <Loader />}
 			<div>
-                <Batches />
+				<Batches />
 			</div>
 		</div>
 	);
--- src/container/BatchDetails/BatchDetails.js
+++ src/container/BatchDetails/BatchDetails.js
@@ -1,47 +1,11 @@
-import React, { useState } from 'react';
-import { Snackbar } from '@material-ui/core';
-import { Alert } from '@material-ui/lab';
-import Loader from '../../component/Loader';
+import React from 'react';
 import '../../styles/batchdetails.scss';
 import Batch from '../../component/Batches/Batch';
 
 const BatchDetails = () => {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [successMsg, setSuccessMsg] = useState('');
-	const [errorMsg, setErrorMsg] = useState('');
 
 	return (
 		<div>
-			<Snackbar
-				open={Boolean(successMsg)}
-				autoHideDuration={3000}
-				onClose={() => setSuccessMsg('')}>
-				<Alert
-					elevation={6}
-					variant='filled'
-					onClose={() => {
-						setSuccessMsg('');
-					}}
-					severity='success'>
-					{successMsg}
-				</Alert>
-			</Snackbar>
-			<Snackbar
-				open={Boolean(errorMsg)}
-				autoHideDuration={3000}
-				onClose={() => setErrorMsg('')}>
-				<Alert
-					elevation={6}
-					variant='filled'
-					onClose={() => {
-						setErrorMsg('');
-					}}
-					severity='error'>
-					{errorMsg}
-				</Alert>
-			</Snackbar>
-			{loading && <Loader />}
 			<div>
                 <Batch />
 			</div>
